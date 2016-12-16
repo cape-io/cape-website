f8bbc6f43110c9ebc9b548c4d223b2600b273df2
Kai Curry <kai@cape.io>
2016-12-16 16:43:23 -0600

    upload fire action

--- src/fire/actions.js
+++ src/fire/actions.js
@@ -1,6 +1,8 @@
-import { noopAction } from 'cape-redux'
+import { createAction, noopAction } from 'cape-redux'
 
 export const AUTH = 'fire/AUTH'
 export const auth = noopAction(AUTH)
 export const LOGOUT = 'fire/LOGOUT'
 export const logout = noopAction(LOGOUT)
+export const UPLOAD_FILE = 'fire/UPLOAD_FILE'
+export const uploadFile = createAction(UPLOAD_FILE)
